d8eb14390c830029c86121e758aff7d5852f6d88
Hellowalls1 <thesingingbutcher@gmail.com>
2020-06-03 13:18:44 -0500

    finished music history i think

--- music-history.sql
+++ music-history.sql
@@ -83,12 +83,6 @@
 --	7)
 --INSERT INTO Artist (ArtistName, YearEstablished) VALUES ('Billie Eilish', 2016);
 
---15)
---SELECT album.Title, album.AlbumLength
---FROM Album album
---WHERE album.AlbumLength = {
---	SELECT MAX(AlbumLength) FROM album
---	};
 
 --8)
 --INSERT INTO ALBUM (Title, ReleaseDate, AlbumLength, Label, ArtistId, GenreId) VALUES ('Billie Eilish Record', 2017, 2198, 'Her Label', 28, 4);
@@ -115,3 +109,79 @@
 --From Album album
 --LEFT JOIN Song song ON album.Id = song.AlbumId
 --GROUP BY (album.Title);
+
+--12)
+--Select 
+--	artist.ArtistName,
+--	Count(song.Id) AS 'Number of Songs'
+--	FROM Artist artist
+--	LEFT JOIN Song song ON song.ArtistId = artist.Id
+--	GROUP BY (artist.ArtistName);
+
+--Write a SELECT statement to display how many songs exist for each genre. 
+--You'll need to use the COUNT() function and the GROUP BY keyword sequence.
+
+--13)
+--Select
+--	genre.Label,
+--	Count(song.Id) AS 'Number of Songs'
+--	FROM Genre genre 
+--	LEFT JOIN Song song ON song.GenreId = genre.Id
+--	GROUP BY (genre.Label);
+
+--Write a SELECT query that lists the Artists that have put out records on more than one record label. 
+--Hint: When using GROUP BY instead of using a WHERE clause, use the HAVING keyword
+
+
+--14)
+--Select 
+--	artist.ArtistName,
+--	COUNT(DISTINCT album.Label) AS 'Number Of Labels'
+--From Album album
+--Left Join Artist artist ON  artist.id = album.ArtistId
+--GROUP BY artist.ArtistName
+--HAVING COUNT(DISTINCT album.Label) > 1;
+	
+
+--15)	
+
+--SELECT
+--album.Title,
+--album.AlbumLength
+--FROM Album album
+--WHERE album.AlbumLength =
+--	(
+--	SELECT MAX(AlbumLength) FROM album
+--	);
+
+
+--Using MAX() function, write a select statement to find the song with the longest duration. 
+--The result should display the song title and the duration.
+
+
+16)
+
+SELECT
+
+song.Title,
+song.SongLength
+From Song song
+WHERE song.SongLength = 
+	(
+	SELECT MAX(SongLength) FROM song
+	);
+
+
+
+17)	
+SELECT
+
+	album.Title AS 'Album Title',
+	song.Title AS 'Song Title',
+	song.SongLength
+	From Album album
+	LEFT JOIN Song song ON song.AlbumId = album.id
+	WHERE song.SongLength = 
+	(
+	SELECT MAX(SongLength) FROM song
+	);
